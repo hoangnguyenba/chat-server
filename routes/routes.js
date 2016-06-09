5d27228758cd067e7b6f5e4e466d110aaaeede80
Hoang Nguyen <hoang.nguyenba@hotmail.com>
2016-06-09 16:49:11 +0700

    Update cors

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 var MessageModel = require("../models/message-model");
+
+var cors = require('cors');
  
 var appRouter = function(app) {
  
-    app.get("/fetch", function(req, res) {
+    app.get("/fetch", cors(), function(req, res) {
         MessageModel.getAll(req.query.thread_id,function(error, result) {
             if(error) {
                 return res.status(400).send(error);
