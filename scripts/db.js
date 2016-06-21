47527436834a36b97fb5090a0428ec84de71e1ff
Hoang Nguyen <hoang.nguyenba@hotmail.com>
2016-06-21 17:44:29 +0700

    Update script db

--- scripts/db.js
+++ scripts/db.js
@@ -7,28 +7,55 @@ var ARR_TABLE = ["Message", "User"];
 var type = "reset"; // create, delete, sample, reset
 var table = "all"; // all, Message, User
 
-if(typeof process.argv[2] !== "undefined")
+if(typeof process.argv[3] !== "undefined")
 {
     console.log('####1####');
-    type = process.argv[2];
+    type = process.argv[3];
 }
 
-if(typeof process.argv[3] !== "undefined")
+if(typeof process.argv[2] !== "undefined")
 {
     console.log('####2####');
     table = process.argv[2];
 }
 
+var tables = [];
 if(table === "all")
 {
-    ARR_TABLE.forEach((t) => {
+    tables = ARR_TABLE;
+}
+else
+{
+    tables.push(table);
+}
+
+tables.forEach((t) => {
+
+    if(type == "reset")
+    {
         console.log('####Delete####');
         require( "./" + t.toLowerCase() + '-table-delete.js');
         console.log('####Create####');
         require( "./" + t.toLowerCase() + '-table-create.js');
         console.log('####Add sample data####');
         require( "./" + t.toLowerCase() + '-table-sample.js');
-    });
-}
+    }
+    else if(type == "create")
+    {
+        console.log('####Create####');
+        require( "./" + t.toLowerCase() + '-table-create.js');
+    }
+    else if(type == "delete")
+    {
+        console.log('####Delete####');
+        require( "./" + t.toLowerCase() + '-table-delete.js');
+    }
+    else if(type == "sample")
+    {
+        console.log('####Add sample data####');
+        require( "./" + t.toLowerCase() + '-table-sample.js');
+    }
+    
+});
 
 console.log('####end####');
